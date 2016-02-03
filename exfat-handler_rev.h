047b97194ba9ddd2f1c7daba2909d1ee0fcf05ae
Fredrik Wikstrom <fredrik@a500.org>
2016-02-03 21:45:24 +0200

    exfat-handler 53.3 (3.2.2016)
    - Disk I/O layer now uses a segmented LRU for the read cache and automatically
      sorted block ranges for the write cache.
    
    - Replaced cache semaphore with a simple boolean variable that indicates to
      the memhandler when the cache is being accessed.

--- exfat-handler_rev.h
+++ exfat-handler_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		53
-#define REVISION	2
-#define DATE		"20.12.2015"
-#define VERS		"exfat-handler 53.2"
-#define VSTRING		"exfat-handler 53.2 (20.12.2015)\r\n"
-#define VERSTAG		"\0$VER: exfat-handler 53.2 (20.12.2015)"
+#define REVISION	3
+#define DATE		"3.2.2016"
+#define VERS		"exfat-handler 53.3"
+#define VSTRING		"exfat-handler 53.3 (3.2.2016)\r\n"
+#define VERSTAG		"\0$VER: exfat-handler 53.3 (3.2.2016)"
