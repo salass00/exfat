f77e00b4b165d5a65ce58b21c89ac49f126573aa
Fredrik Wikstrom <fredrik@a500.org>
2026-03-04 21:37:39 +0200

    Added exFATFileSystem revision files.

---
+++ exFATFileSystem_rev.h
@@ -0,0 +1,6 @@
+#define VERSION		53
+#define REVISION	3
+#define DATE		"3.2.2016"
+#define VERS		"exFATFileSystem 53.3"
+#define VSTRING		"exFATFileSystem 53.3 (3.2.2016)\r\n"
+#define VERSTAG		"\0$VER: exFATFileSystem 53.3 (3.2.2016)"
